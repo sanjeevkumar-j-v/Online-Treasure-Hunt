0a01faa5b0cf01bfda223dbd788ac3b25dfc8b06
sanjeev kumar <sanjeevkumarjv@gmail.com>
2020-05-06 01:22:56 +0530

    Add git ignore, procfile and update

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
-const port = 8000;
+let port = process.env.PORT;
+if(port == null || port == ""){
+    port = 8000;
+}
 
 app.use(express.urlencoded());
 
