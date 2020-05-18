846f1cab3cf3d204a5bb700cd9fa366ee8dd8be4
sanjeev kumar <sanjeevkumarjv@gmail.com>
2020-05-19 01:28:09 +0530

    mailer - allow access with a html message without user name

--- mailers/registration_mailer.js
+++ mailers/registration_mailer.js
@@ -4,13 +4,13 @@ const nodeMailer = require('../config/nodemailer');
 
 exports.accCreated = (user) => {
     let htmlString = nodeMailer.renderTemplate({user: user}, '/reg/reminder.ejs')
-    htmlString = "<h4>Hey "+user.name+ ",</h4> <br> <h1>Your registration for the online treasure hunt is confirmed.</h1>"
+
     console.log("At acc created - user", user, user.email)
     nodeMailer.transporter.sendMail({
         from: 'sanjeevkumarjv@gmail.com',
         to: user.email,
         subject: "Inno Hunt",
-        html: htmlString
+        html: "<h4>Hey there, </h4> <br> <h1>Your registration for the online treasure hunt is confirmed.</h1>"//htmlString
     }, (err, info) => {
         if(err) { console.log('Error in sending mail', err); return;}
 
