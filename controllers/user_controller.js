72da6a444a2457463e26c619849ed837d38bab46
sanjeev kumar <sanjeevkumarjv@gmail.com>
2020-05-25 15:16:05 +0530

    close registrations

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -15,41 +15,40 @@ module.exports.signIn = function(req, res){
     });
 }
 
-module.exports.signUp = function(req, res){
-    if(req.isAuthenticated()){
-        // return res.redirect('/playground');
-        return res.redirect('/playground/');
+// module.exports.signUp = function(req, res){
+//     if(req.isAuthenticated()){
+//         return res.redirect('/playground/');
 
-    }
+//     }
 
-    return res.render('sign_up',{
-        title: "innohunt sign-up"
-    });
-}
+//     return res.render('sign_up',{
+//         title: "innohunt sign-up"
+//     });
+// }
 
 
 // get the sign up data
-module.exports.create = function(req, res){
-    if (req.body.password != req.body.confirm_password) {
-        return res.redirect('back');
-    }
+// module.exports.create = function(req, res){
+//     if (req.body.password != req.body.confirm_password) {
+//         return res.redirect('back');
+//     }
 
-    User.findOne({email: req.body.email}, function(err, user){
-        if(err){console.log('Error in finding user in signing up'); return;}
+//     User.findOne({email: req.body.email}, function(err, user){
+//         if(err){console.log('Error in finding user in signing up'); return;}
 
-        if(!user) {
-            User.create(req.body, function(err, user){
-                if(err){console.log('Error in creating user in signing up'); return;}
-                // console.log('User created : ',user);
-                // registrationMailer.accCreated(user);
-                return res.redirect('/users/sign-in');
-            })
-        }else{
-            return res.redirect('back');
-        }
+//         if(!user) {
+//             User.create(req.body, function(err, user){
+//                 if(err){console.log('Error in creating user in signing up'); return;}
+//                 // console.log('User created : ',user);
+//                 // registrationMailer.accCreated(user);
+//                 return res.redirect('/users/sign-in');
+//             })
+//         }else{
+//             return res.redirect('back');
+//         }
 
-    });
-}
+//     });
+// }
 
 
 
--- routes/users.js
+++ routes/users.js
@@ -6,10 +6,10 @@ const userController = require('../controllers/user_controller');
 
 router.get('/wait', userController.wait);
 router.get('/sign-in',userController.signIn);
-router.get('/sign-up',userController.signUp);
+// router.get('/sign-up',userController.signUp);
 router.get('/sign-out', userController.destroySession);
 
-router.post('/create', userController.create);
+// router.post('/create', userController.create);
 router.post('/create-session', passport.authenticate(
     'local',
     {failureRedirect: '/users/sign-in'},
